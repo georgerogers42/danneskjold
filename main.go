1cb2814ce75c6ec170fd5436ccde1ee6bf52000d
George Rogers <grogers385@gmail.com>
2014-10-24 13:00:38 -0500

    update

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"github.com/georgerogers42/danneskjold/repo"
 	"net/http"
+	"fmt"
 	"flag"
 )
 
 func main() {
-	bind := flag.String("bind", "Port to bind to", ":8080")
+	var bind string
+	flag.StringVar(&bind, "bind", ":8080", "Address To Bind To")
 	flag.Parse()
+	fmt.Println("Binding on ---", bind)
 	http.Handle("/", repo.App)
-	err := http.ListenAndServe(*bind, nil)
+	err := http.ListenAndServe(bind, nil)
 	panic(err)
 }
